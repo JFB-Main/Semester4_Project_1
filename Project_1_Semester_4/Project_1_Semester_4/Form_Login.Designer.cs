9cecb5c955c733f2df60aa5a3756430ed8a81b79
Javier Farel Baroto <javierfarelb@gmail.com>
2025-02-28 16:49:01 +0700

    Menambahkan Inisialisasi project

---
+++ Project_1_Semester_4/Project_1_Semester_4/Form_Login.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Project_1_Semester_4
+{
+    partial class Form_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txUsername = new System.Windows.Forms.TextBox();
+            this.txPassword = new System.Windows.Forms.TextBox();
+            this.bt_Login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txUsername
+            // 
+            this.txUsername.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(14)))));
+            this.txUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txUsername.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txUsername.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(253)))), ((int)(((byte)(0)))));
+            this.txUsername.Location = new System.Drawing.Point(368, 248);
+            this.txUsername.Name = "txUsername";
+            this.txUsername.Size = new System.Drawing.Size(540, 70);
+            this.txUsername.TabIndex = 0;
+            this.txUsername.TextChanged += new System.EventHandler(this.txUsername_TextChanged);
+            // 
+            // txPassword
+            // 
+            this.txPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(14)))), ((int)(((byte)(14)))));
+            this.txPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txPassword.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txPassword.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(253)))), ((int)(((byte)(0)))));
+            this.txPassword.Location = new System.Drawing.Point(368, 428);
+            this.txPassword.Name = "txPassword";
+            this.txPassword.PasswordChar = '*';
+            this.txPassword.Size = new System.Drawing.Size(540, 70);
+            this.txPassword.TabIndex = 5;
+            this.txPassword.TextChanged += new System.EventHandler(this.txPassword_TextChanged);
+            // 
+            // bt_Login
+            // 
+            this.bt_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(253)))), ((int)(((byte)(0)))));
+            this.bt_Login.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Login.Location = new System.Drawing.Point(513, 571);
+            this.bt_Login.Name = "bt_Login";
+            this.bt_Login.Size = new System.Drawing.Size(258, 49);
+            this.bt_Login.TabIndex = 2;
+            this.bt_Login.Text = "LOG IN";
+            this.bt_Login.UseVisualStyleBackColor = false;
+            this.bt_Login.Click += new System.EventHandler(this.bt_Login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(363, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 28);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "USERNAME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(363, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PASSWORD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(253)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(257, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(775, 83);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "ADMINISTRATION LOG IN";
+            // 
+            // Form_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project_1_Semester_4.Properties.Resources.gridbg;
+            this.ClientSize = new System.Drawing.Size(1283, 752);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_Login);
+            this.Controls.Add(this.txPassword);
+            this.Controls.Add(this.txUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "_";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txUsername;
+        private System.Windows.Forms.TextBox txPassword;
+        private System.Windows.Forms.Button bt_Login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Project_1_Semester_4/Project_1_Semester_4/Form_Login.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1_Semester_4
+{
+    public partial class Form_Login : Form
+    {
+        public Form_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void txUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_Login_Click(object sender, EventArgs e)
+        {
+            Form_MainMenu form_MainMenuLink = new Form_MainMenu();
+            form_MainMenuLink.Show();
+            this.Hide();
+        }
+    }
+}
